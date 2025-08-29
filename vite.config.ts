83abea2653773c3a946258159027e2508ffc157a
pepe <gsuper.66522@gmail.com>
2025-08-29 00:03:47 -0400

    router fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-modulo7",
+  base: "/react-modulo7/",
 });
